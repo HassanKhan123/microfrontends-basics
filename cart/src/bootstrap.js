71a68dc5b274b71891674921f688d1508b270ead
HassanKhan123 <hk2602809@gmail.com>
2021-06-04 22:36:24 +0500

    refactored cart

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,7 +1,19 @@
 import faker from 'faker';
 
-const cartText = `<div>
+const mount = el => {
+  const cartText = `<div>
     You have ${faker.random.number()} items in your cart.
-</div>`;
+    </div>`;
 
-document.querySelector('#dev-cart').innerHTML = cartText;
+  el.innerHTML = cartText;
+};
+
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#dev-cart');
+
+  if (el) {
+    mount(el);
+  }
+}
+
+export { mount };
